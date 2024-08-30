9913021de8566fa1a9decc00fa880d6d02339342
thxssio <thxssio@gmail.com>
2024-08-29 21:40:29 -0300

    Working

---
+++ src/amcl_package/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+package_name = 'amcl_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='thxssio',
+    maintainer_email='thxssio@gmail.com',
+    description='AMCL package',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
